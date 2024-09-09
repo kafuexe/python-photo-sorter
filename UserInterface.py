853a4fbd417a21c69ba1748e72bb417fdd2e84fb
kafuexe <ofeki884@gmail.com>
2024-09-09 16:39:15 +0300

    added input and outputboxs

--- UserInterface.py
+++ UserInterface.py
@@ -9,29 +9,43 @@ class App(tk.Tk):
         self.geometry("900x640")
         self.title("CyberChaperone")
 
+        # input section ----------------------------------------------------------
         self.button_change_input_dir = tk.Button(
-            self, text="Browse Files", command=self.change_input_dir
+            self,
+            text="Browse Files",
+            command=lambda: self.BrowseFiles(self.textbox_input_dir),
         )
-
-        self.button_exit = tk.Button(self, text="Exit", command=exit)
-
         self.textbox_input_dir = tk.Entry(self, width=50, bg="light yellow")
 
+        # output section --------------------------------------------------------
+        self.button_change_output_dir = tk.Button(
+            self,
+            text="Browse Files",
+            command=lambda: self.BrowseFiles(self.textbox_output_dir),
+        )
+        self.textbox_output_dir = tk.Entry(self, width=50, bg="light yellow")
+
+        # other ----------------------------------------------------------------
+        self.button_exit = tk.Button(self, text="Exit", command=exit)
+
         self.textbox_input_dir.grid(row=1, column=1)
-        self.button_change_input_dir.grid(column=1, row=2)
+        self.button_change_input_dir.grid(row=1, column=2)
+
+        self.textbox_output_dir.grid(row=2, column=1)
+        self.button_change_output_dir.grid(row=2, column=2)
+
         self.button_exit.grid(column=1, row=3)
         self.mainloop()
 
-    def change_input_dir(self):
+    def BrowseFiles(self, textbox):
         filename = filedialog.askdirectory(
             initialdir="/",
             title="Select a folder",
         )
-
         # Change label contents
         print(f"Selected {filename} as input directory")
-        self.textbox_input_dir.delete(0, "end")
-        self.textbox_input_dir.insert(0, filename)
+        textbox.delete(0, "end")
+        textbox.insert(0, filename)
 
 
 x = App()
