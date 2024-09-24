0a573b2f365395e0f24ebe9369d509bbde651cd9
kafuexe <ofeki884@gmail.com>
2024-09-24 18:55:30 +0300

    added properties to the ini file creation process

--- UserInterface.py
+++ UserInterface.py
@@ -78,6 +78,7 @@ class App(tk.Tk):
             self.config.set("main", "textbox_input_dir", "")
             self.config.set("main", "textbox_output_dir", "")
             self.config.set("main", "textbox_input_format", "")
+            self.config.set("main", "checkbutton_unknowdata_checkvar", "0")
 
             self.config.write(open(self.initfile, "w"))
 
